3baea1b19dcebde3131c23481f61e26595b4683e
David <oyewumidavid84@gmail.com>
2022-09-11 03:23:08 +0100

    make it better

--- api/client/src/utils.js
+++ api/client/src/utils.js
@@ -1,5 +1,5 @@
 import axios from 'axios'
 
 export const axiosInstance = axios.create({
-    baseURL: "https://chasebankapp.herokuapp.com/api"
+    baseURL: "https://chasebankapp.herokuapp.com/api/"
 })
